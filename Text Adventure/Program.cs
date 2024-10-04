8435fa99966bb48b57ee33df33373785b6bb68d3
MUKILTEO\1602247 <krestonridge@icloud.com>
2024-10-04 12:25:02 -0700

    <Fixes, Organization, New Beeps> -I added a new void for both choices and the players choice -I reorganized the code to be more readable -I added the response to the player -I added a beep when each choice pops up

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -20,43 +20,59 @@ namespace Text_Adventure
             Thread.Sleep(400);
             Console.BackgroundColor = ConsoleColor.Green;
             Console.ForegroundColor = ConsoleColor.Black;
-            Console.WriteLine(":|KRESTON CONSOLE 2000|:\n");
+            Console.WriteLine(":|ARTEMIS CONSOLE|:\n");
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Gray;
-            Thread.Sleep(1500);
-            Choice1();
+            Thread.Sleep(3500);
+            choices001();
         }
 
-        private static void Choice1()
+        private static void choices001()
         {
-            Console.WriteLine("What Room Do You DARE Enter On This Cold, Dark, Rainy Day...");
+            Console.WriteLine("What Room Do You DARE Enter On This Cold, Dark, Rainy Day...\n");
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.WriteLine("[d]  The Dark Room\n");
-            Console.WriteLine("[l]  The Room With Just a Lightbulb\n");
+            Thread.Sleep(4000);
+            Console.Beep(700, 200);
+            Console.Write("[d]  The Dark Room With The "); Console.ForegroundColor = ConsoleColor.DarkBlue; Console.Write("Water"); Console.ForegroundColor = ConsoleColor.DarkGray; Console.WriteLine(" On The Floor\n");
+            Thread.Sleep(2000);
+            Console.Beep(700, 200);
+            Console.Write("[l]  The Room With Just A "); Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("Lightbulb\n"); Console.ForegroundColor = ConsoleColor.DarkGray;
+            Thread.Sleep(2000);
+            Console.Beep(700, 200);
             Console.Write("[b]  The "); Console.ForegroundColor = ConsoleColor.Red; Console.Write("Bloody"); Console.ForegroundColor = ConsoleColor.DarkGray; Console.WriteLine(" Room\n");
-            Console.WriteLine("[c]  The Clown Room");
-
+            Thread.Sleep(2000);
+            Console.Beep(700, 200);
+            Console.Write("[c]  The "); Console.ForegroundColor = ConsoleColor.Red; Console.Write("C"); Console.ForegroundColor = ConsoleColor.DarkYellow; Console.Write("l"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("o"); Console.ForegroundColor = ConsoleColor.Blue; Console.Write("w"); Console.ForegroundColor = ConsoleColor.DarkMagenta; Console.Write("n"); Console.ForegroundColor = ConsoleColor.DarkGray; Console.Write(" Room\n\n");
+            Thread.Sleep(500);
+            Console.Write("  .   .   .  ");
+            choice1();
+        }
+        private static void choice1()
+        {
             String input = Console.ReadLine();
             if (input == "d")
             {
-                Console.WriteLine("");
+                Console.Write("\n\nYou Enter The Dark Room With The "); Console.ForegroundColor = ConsoleColor.DarkBlue; Console.Write("Water"); Console.ForegroundColor = ConsoleColor.DarkGray; Console.WriteLine(" On The Floor\n");
             }
             else if (input == "l")
             {
-                Console.WriteLine("");
+                Console.Write("\n\nYou Enter The Room With Just A "); Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine("Lightbulb\n"); Console.ForegroundColor = ConsoleColor.DarkGray;
             }
             else if (input == "b")
             {
-                Console.WriteLine("");
-            }
+                Console.Write("\n\nYou Enter The "); Console.ForegroundColor = ConsoleColor.Red; Console.Write("Bloody"); Console.ForegroundColor = ConsoleColor.DarkGray; Console.WriteLine(" Room\n");
+             }
             else if (input == "c")
             {
-                Console.WriteLine("");
+                Console.Write("\n\nYou Enter The "); Console.ForegroundColor = ConsoleColor.Red; Console.Write("C"); Console.ForegroundColor = ConsoleColor.DarkYellow; Console.Write("l"); Console.ForegroundColor = ConsoleColor.Green; Console.Write("o"); Console.ForegroundColor = ConsoleColor.Blue; Console.Write("w"); Console.ForegroundColor = ConsoleColor.DarkMagenta; Console.Write("n"); Console.ForegroundColor = ConsoleColor.DarkGray; Console.Write(" Room\n\n");
             }
             else
             {
-                Console.WriteLine("That was not an option");
+                Console.WriteLine("pick again... [d] | [l] | [b] | [c]\n\n");
+                Thread.Sleep(500);
+                Console.Write("  .   .   .  ");
+                choice1();
             }
         }
     }
@@ -73,7 +89,7 @@ namespace Text_Adventure
 //Console.BackgroundColor = ConsoleColor.*;*color                                                        ---    Background Color for text
 //Console.ForegroundColor = ConsoleColor.*;*color                                                        ---    Color of text
 //Console.WriteLine("*");*text                                                                           ---    <<<Write Text
-//Console.WriteLine("*");*text, BUT, it makes the NEXT text text on the same line                        ---    Witing text on same line
+//Console.Write("*");*text, BUT, it makes the NEXT text text on the same line                            ---    Witing text on same line
 //Console.WriteLine("*\r");*return (deletes the letter beforehand)                                       ---    "Return" for text
 //Console.WriteLine("*\t");*tab                                                                          ---    Tab for text
 //Console.WriteLine("*\n");*new line/ indent                                                             ---    Indent for text>>>
@@ -92,17 +108,6 @@ namespace Text_Adventure
 //    
 //}
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 //
 //
 //
